ced35fb0032bc3066146f243ab1e9c6ba2dd0412
Jeevanjot Singh Vital <40867747+genievot@users.noreply.github.com>
2022-06-14 17:09:55 +0530

    Update merkleTreeHistoryTest.js

--- test/merkleTreeHistoryTest.js
+++ test/merkleTreeHistoryTest.js
@@ -37,15 +37,11 @@ describe('merkleTreeHistoryTest', () => {
   });
 
   it('bytes to int test', async () => {
-    // const get = await contract.methods.t5_now({ gas: 5000000, gasPrice: 20000000000 });
-    // const get = await contract.methods.mimcFeistel(5, 3, 0, { gas: 30000000, gasPrice: 2000000000 });
-    // const get = await contract.methods.get_feistel_twice({ gas: 30000000, gasPrice: 2000000000 });
-    const field_size_to_int = await contract.methods.field_size_to_int({ gas: 30000000, gasPrice: 2000000000 });
-    console.log(field_size_to_int.decodedResult)
+//     const field_size_to_int = await contract.methods.field_size_to_int({ gas: 30000000, gasPrice: 2000000000 });
+//     console.log(field_size_to_int.decodedResult)
     const zeros_to_find = await contract.methods.zeros_to_find({ gas: 30000000, gasPrice: 2000000000 });
     console.log(zeros_to_find.decodedResult)
-    // inputs:   "left": 565656512323255, "right": 465468698741321
-    // 1st output expected: 38891872867026040328108358595221420665472868284040096954349764956244819173849 & 44960690674927008188172993140266778298953095954586356598642382665837691415817 (as produced by circom lib)
+    console.log(zeros_to_find.decodedResult.length)
   });
 
 });
